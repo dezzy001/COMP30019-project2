4cb948e29aa2d1c3724f660f1f12d3afa4cbf317
Derek <dezzy001@gmail.com>
2017-09-07 18:22:14 +1000

    Have a basic UI working

---
+++ Assets/scripts/SceneManagement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour {
+
+	void Start(){
+		//allows app to run in background, multitask
+		Application.runInBackground = true; 
+	}
+
+	public void changeScene(string sceneName){
+
+		SceneManager.LoadScene ( sceneName);
+
+	}
+}
---
+++ Assets/scripts/start scene scripts/StartScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//need to include the UI components
+using UnityEngine.UI;
+
+
+
+public class StartScript : MonoBehaviour {
+
+	/*Important things to clarify
+	1)Anchor everything in UI - so all objects are responsive to resolution changes
+	2)bottom to top ordering in UI: bottom game objects will overylay game objects above it (same hierarchy)*/
+
+
+	public GameObject startPanel;
+	public GameObject mainMenuPanel;
+
+	// Use this for initialization
+	void Start () {
+
+		//set all panels to false, other than the start Panel to true
+		//do not show
+		mainMenuPanel.SetActive (false);
+		//show
+		startPanel.SetActive (true);
+	}
+
+	public void clickToMainMenu(){
+		startPanel.SetActive (false);
+		mainMenuPanel.SetActive (true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
