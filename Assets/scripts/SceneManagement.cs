66e83e20ce5e62976296e7292c951443d9232484
Derek <dezzy001@gmail.com>
2017-09-08 23:57:09 +1000

    added a loading screen to all scenes

--- Assets/scripts/SceneManagement.cs
+++ Assets/scripts/SceneManagement.cs
@@ -4,17 +4,63 @@ using UnityEngine;
 
 
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneManagement : MonoBehaviour {
+
+
+	public GameObject loadingPanel;
+	public Slider loadingBarSlider;
+
 
 	void Start(){
 		//allows app to run in background, multitask
 		Application.runInBackground = true; 
+
+		//only have this active when loading
+		loadingPanel.SetActive (false);
 	}
+
 
 	public void changeScene(string sceneName){
 
-		SceneManager.LoadScene ( sceneName);
+
+
+		StartCoroutine (LoadAsynchronously(sceneName));
+
+		//SceneManager.LoadScene (sceneName);
+
+		//AsyncOperation operation = SceneManager.LoadSceneAsync (sceneName);
+
+
+	}
+
+
+	IEnumerator LoadAsynchronously(string sceneName){
+
+		//open up the loading panel
+		loadingPanel.SetActive (true);
+		//the scenes are so simple... they load in a split second, make it wait x secs to demonstrate loading page
+		yield return new WaitForSeconds (2);
+
+		//preloads the next scene, while still in the current scene
+		AsyncOperation operation = SceneManager.LoadSceneAsync (sceneName);
+
+
+
+		//while the next scene has not loaded , show the loading progress
+		while(!operation.isDone){
+
+
+			//unity loads in 2 stages, (1) loading 0 - 0.9 , (2)activation 0.9 - 10(deletes old stuff and replaces with new scene)
+			float progress = Mathf.Clamp01 (operation.progress / 0.9f);
+
+			loadingBarSlider.value = progress;
+
+			yield return null;
+		}
+
+
 
 	}
 }
