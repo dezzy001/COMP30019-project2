a0f0595e62e301b7955571b99cb1f10a4a78243d
Derek <dezzy001@gmail.com>
2017-09-16 22:11:36 +1000

    added a quit panel in the camp and hacking game scenes

---
+++ Assets/_scripts/camp scripts/QuitPanel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitPanel : MonoBehaviour {
+
+	public GameObject quitPanel;
+
+	private bool switchOn = false;
+
+	// Use this for initialization
+	void Start () {
+
+		quitPanel.SetActive (false);	
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyDown("escape")){
+			switchOn = !switchOn;
+			quitPanel.SetActive (switchOn);
+
+		}
+
+	}
+}
