e56dd0e73b9509a52e8b0517786308cf7aff9b66
Steven Lee <stevenlee090@gmail.com>
2017-09-19 12:08:02 +1000

    updated and fixed sound effects, working properly now

--- Assets/_scripts/hacking game scripts/DestroyIfOffscreen.cs
+++ Assets/_scripts/hacking game scripts/DestroyIfOffscreen.cs
@@ -4,8 +4,8 @@ using System.Collections;
 public class DestroyIfOffscreen : MonoBehaviour {
     
 	// Triggered as soon as the object is outside of the camera frustrum
-	void OnBecameInvisible () {
-        Destroy(this.gameObject);
-	}
+//	void OnBecameInvisible () {
+//        Destroy(this.gameObject);
+//	}
 
 }
--- Assets/_scripts/hacking game scripts/Player Script/ProjectileController.cs
+++ Assets/_scripts/hacking game scripts/Player Script/ProjectileController.cs
@@ -10,42 +10,92 @@ public class ProjectileController : MonoBehaviour {
     public string tagToDamage = "Enemy";
 	public string tagUntouchableEnemys = "EnemyUntouchable";
 
+	public AudioSource audioSource;
+	public AudioClip[] audioClip;
+
+	public float timeToLive;
+
+	private int laserFireSound = 0;
+	private int bulletHitSound = 1;
+
 	void Start(){
-		/*
-		MeshRenderer renderer = this.gameObject.GetComponent<MeshRenderer>();
-		renderer.material.color = Color.yellow;
-		*/
+
+		audioSource = GetComponent<AudioSource> ();
+
+		// Play the laser firing sound
+		audioSource.PlayOneShot (audioClip [laserFireSound], 0.8f);
+
+		// MeshRenderer renderer = this.GetComponent<MeshRenderer> ();
+		// renderer.material.color = Color.yellow;
+
 
 	}
 
     // Update is called once per frame
     void Update () {
+
         this.transform.Translate(velocity * Time.deltaTime);
 
+		timeToLive -= Time.deltaTime;
+
+		if (timeToLive <= 0.0f) {
+			Destroy (this.gameObject);
+		}
 
 	}
 
     // Handle collisions
     void OnTriggerEnter(Collider col){
 
+		// problem reason : projectile dies before sound can be heard
+		// fix : implement sound so that it does not die when projectile dies
+
+//		if (col.gameObject.tag != "Player") {
+//			audioSource.PlayOneShot (audioClip [1], 0.8f);
+//		}
+
+
         if (col.gameObject.tag == tagToDamage){
 			
 			// Damage object with relevant tag
 			HealthManager healthManager = col.gameObject.GetComponent<HealthManager>();
 			healthManager.ApplyDamage(damageAmount);
-            
-			// Destroy self
-            Destroy(this.gameObject);
+
+			PlaySoundOneShot (bulletHitSound);
 
         }
 
 		//cannot damage these enemys
 		if(col.gameObject.tag == tagUntouchableEnemys){
 
-			// Destroy self
-			Destroy(this.gameObject);
+			PlaySoundOneShot (bulletHitSound);
 
 		}
 
     }
+
+	// Hnadle sound effect playback (playOneShot) and Destroy obj after playback finishes
+	void PlaySoundOneShot(int clip_index) {
+		
+		audioSource.pitch = 1;
+		audioSource.PlayOneShot (audioClip [clip_index], 0.8f);
+
+		MeshRenderer m = this.GetComponent<MeshRenderer> ();
+		m.enabled = false;
+
+		BoxCollider b = this.GetComponent<BoxCollider> ();
+		b.enabled = false;
+
+		// Destroy self
+		Destroy(this.gameObject, audioClip [clip_index].length);
+
+	}
+
+	// Handle sound effect playback
+	void PlaySound(int clip_index) {
+		AudioSource audio = GetComponent<AudioSource> ();
+		audio.clip = audioClip [clip_index];
+		audio.Play ();
+	}
+
 }
