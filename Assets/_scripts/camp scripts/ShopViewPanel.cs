5be07a977309caf3e2884c2af3cef28c4f6f400e
Derek <dezzy001@gmail.com>
2017-09-21 18:36:22 +1000

    polished shop and added 3 new tabs

---
+++ Assets/_scripts/camp scripts/ShopViewPanel.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class ShopViewPanel : MonoBehaviour {
+
+	public Button chipsButton;
+	public GameObject chipsPanel;
+
+	public Button skillsButton;
+	public GameObject skillsPanel;
+
+	public Button skinsButton;
+	public GameObject skinsPanel;
+
+
+	private ArrayList allTabPanels;
+
+
+	void Start(){
+
+		allTabPanels = new ArrayList ();
+
+		allTabPanels.Add (chipsPanel);
+		allTabPanels.Add (skillsPanel);
+		allTabPanels.Add (skinsPanel);
+
+		chipsButton.onClick.AddListener (() => switchPanelTab( chipsPanel));
+		skillsButton.onClick.AddListener (() => switchPanelTab( skillsPanel));
+		skinsButton.onClick.AddListener (() => switchPanelTab( skinsPanel));
+
+	}
+
+	void Update () {
+		
+	}
+
+
+	void switchPanelTab(GameObject panel){
+		//SwitchToPanel.closeAllPanels (allTabPanels);
+		SwitchToPanel.activatePanel (panel,allTabPanels);
+
+	}
+
+}
