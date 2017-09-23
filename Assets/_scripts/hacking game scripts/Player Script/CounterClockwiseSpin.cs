bf97f7dc3263994f8b8b9b14c458c61d964f08da
Steven Lee <stevenlee090@gmail.com>
2017-09-23 12:47:13 +1000

    implemented 7 shooters in test scene

---
+++ Assets/_scripts/hacking game scripts/Player Script/CounterClockwiseSpin.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CounterClockwiseSpin : MonoBehaviour {
+
+	public float spinSpeed;
+
+	// Update is called once per frame
+	void Update () {
+		// Let point light move around the terrain
+		this.transform.localRotation *= Quaternion.AngleAxis(spinSpeed * Time.deltaTime, Vector3.down);
+	}
+}
