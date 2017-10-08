6bcbb92956f88acd909ead8974ddde1de924191e
Derek <dezzy001@gmail.com>
2017-10-08 11:58:54 +1100

    added timer to each level, need to store this

---
+++ Assets/_scripts/hacking game scripts/StopwatchScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+using UnityEngine.UI;
+
+public class StopwatchScript : MonoBehaviour {
+
+	public GameObject hackingCompletePanel;
+	public Text stopwatchText;
+	public Stopwatch timer;
+
+
+
+	// Use this for initialization
+	void Start () {
+		timer = new Stopwatch ();
+		timer.Start();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		stopwatchText.text = timer.Elapsed.ToString();
+
+		if (hackingCompletePanel.activeSelf == true) {
+			timer.Stop();
+		}
+	}
+}
